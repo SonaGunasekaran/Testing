91e089d4b6b1bc4278e8e36ddb67a9d078ec11aa
[Sona] <[sona16061999@gmail.com]>
2021-06-27 19:56:36 +0530

    Modified

--- Program.cs
+++ Program.cs
@@ -2,14 +2,56 @@
 
 namespace Testing
 {
-    class Program
+    class Dog
     {
-        static void Main(string[] args)
+
+        private string dogName;
+        private int dogAge;
+
+        public string Name
         {
-            int varOne = 12;
-            int varTwo = 13;
-            Console.WriteLine(varOne * varTwo);
-            Console.WriteLine("Hello World!");
+            get
+            {
+                return dogName;
+            }
+            set
+            {
+                dogName =value;
+
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return dogAge;
+            }
+            set
+            {
+                 dogAge = value;
+
+            }
         }
     }
-}
+
+    
+
+
+        class Program
+        {
+
+
+            static void Main(string[] args)
+            {
+               Dog myObj = new Dog();
+                
+                myObj.Name = "brown";
+                myObj.Age =12;
+
+                 Console.WriteLine("My dog name is"  + myObj.Name +  "his age is"  + myObj.Age);
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+
